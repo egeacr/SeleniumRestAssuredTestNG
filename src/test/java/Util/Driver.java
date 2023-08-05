ef087fc0144db18f275b9ce6f80cf64251507f62
Ege Acar <egea748@gmail.com>
2023-08-05 14:43:01 +0300

    ChromeOption update(size)

--- src/test/java/Util/Driver.java
+++ src/test/java/Util/Driver.java
@@ -20,12 +20,14 @@ public class Driver {
             if(browser.equalsIgnoreCase("chrome")){
                 WebDriverManager.chromedriver().driverVersion("114.0.5735.90").setup();
                 ChromeOptions options = new ChromeOptions();
-                \\options.addArguments("--headless=new");
+                options.addArguments("--headless=new");
+                //options.addArguments("--headless");
                 options.addArguments("--disable-notifications");
                 options.addArguments("--no-sandbox");
                 options.addArguments("--disable-dev-shm-usage");
                 options.addArguments("--disable-gpu");
                 options.addArguments("--remote-allow-origins=*");
+                options.addArguments("--window-size=1920,1080");
                 driver = new ChromeDriver(options);
             }
             else if (browser.equalsIgnoreCase("firefox")){
